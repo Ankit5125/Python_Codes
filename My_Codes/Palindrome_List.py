d7c3254146d36015872d59ff3daa7ef0dda4396e
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-15 22:31:07 +0530

    Create Palindrome_List.py

---
+++ My_Codes/Palindrome_List.py
@@ -0,0 +1,30 @@
+length = int(input("Enter Length of List :"))
+
+my_list = []
+
+for i in range(length):
+    while True:
+        try:
+            my_list.append(int(input(f"Element {i+1} = ")))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+print("Your List :", my_list)
+
+first = 0
+last = len(my_list) - 1
+
+is_palindrome = True
+
+while first < last:
+    if my_list[first] != my_list[last]:
+        is_palindrome = False
+        break
+    first += 1
+    last -= 1
+
+if is_palindrome:
+    print("True")
+else:
+    print("False")
