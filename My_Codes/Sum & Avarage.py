70a9fba8b726b48826a2d08e6a0c816bc0d6b32a
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-16 14:41:34 +0530

    Create Sum & Avarage.py

---
+++ My_Codes/Sum & Avarage.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter Number 1 : "))
+num2 = int(input("Enter Number 2 : "))
+num3 = int(input("Enter Number 3 : "))
+
+sum = num1 + num2 + num3 
+print("Sum :",sum)
+print("Avarage ",sum/3)
