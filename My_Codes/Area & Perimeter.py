10245f13ae4f8f2803e79bdf3af0788f82a83a23
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-16 14:46:25 +0530

    Update and rename Circle_Area.py to Area & Perimeter.py

---
+++ My_Codes/Area & Perimeter.py
@@ -0,0 +1,13 @@
+# for Circle.......
+# r = int(input("Enter Radius : "))
+
+# print("Radius : " , 3.14 * (r ** 2))
+# print("Perimeter : " , 2 * 3.14 * r)
+
+
+# for Ractangle.....
+s1 = int(input("Enter Side 1 : "))
+s2 = int(input("Enter Side 2 : "))
+
+print("Area : " , s1 * s2)
+print("Perimeter : " , 2 *(s1 + s2))
--- My_Codes/Circle_Area.py
+++
@@ -1,4 +0,0 @@
-r = int(input("Enter Radius : "))
-
-print("Radius : " , 3.14 * (r ** 2))
-print("Perimeter : " , 2 * 3.14 * r)
