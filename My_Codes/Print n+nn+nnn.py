a9973a4b65a4ac2b931cc37c5d98293437a1a639
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-17 15:52:35 +0530

    Update Print n+nn+nnn.py

--- My_Codes/Print n+nn+nnn.py
+++ My_Codes/Print n+nn+nnn.py
@@ -1,3 +1,9 @@
+# all you have to do is....
+# take 'n' from user (obviously as a String)
+# print n + nn + nnn
+# exam -> n = 5 
+# 5 + 55 + 555 = 615
+
 num = int(input("Enter N : "))
 
 nn = int(str(num) + str(num))
