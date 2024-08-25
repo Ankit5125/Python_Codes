875a9b326a612af74e69481273064037772ab061
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-25 18:02:08 +0530

    Update Prime Number.py

--- My_Codes/Prime Number.py
+++ My_Codes/Prime Number.py
@@ -1,8 +1,10 @@
 num = int(input("Enter Number : "))
 
-for i in range (2,int(num/2 + 1)) :
-    if num % i == 0 :
-        print("Not Prime")
-        exit()
-        
-print("Prime")        
+if num == 0:
+    print("Prime")
+else:
+    for i in range (2,int(num/2 + 1)) :
+        if num % i == 0 :
+            print("Not Prime")
+            exit()
+        print("Prime")
