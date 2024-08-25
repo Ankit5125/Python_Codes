e61297555aae24d29cfe5636dafb793e8a2929aa
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-25 13:16:39 +0000

    created Reversed_

---
+++ My_Codes/Reversed Table.py
@@ -0,0 +1,21 @@
+num = int(input("Enter Number : "))
+
+'''
+Method 1 : 
+
+i = 10 
+while i > 0 :
+    print( i * num )
+    i-=1
+
+'''
+
+''' 
+Method 2 : 
+'''
+for i in range(10):
+    print((10-i) * num)
+
+
+
+
