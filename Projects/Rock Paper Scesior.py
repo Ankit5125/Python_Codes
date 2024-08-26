107318397698ed218638a8af538771ed658941f8
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-26 11:00:52 +0000

    updated RPS

--- Projects/Rock Paper Scesior.py
+++ Projects/Rock Paper Scesior.py
@@ -1,25 +1,33 @@
 import random
 
-options = {
-    1:"Rock",
-    2:"Paper",
-    3:"Scissors"
-}
+options = {1: "Rock",2 : "Paper",3 : "Scissor"}
 
+
+def winner(comp,user):
+    if options[comp] == options[user] :
+        print("\nDraw\n")
+        
+    #elif options[comp] ==     
+
+    
 while True :
-    print("Enter Choice : \n 1 -> Play \t 0 -> Exit")
-    go_ahead = int(input("Choice : "))
-
-    if go_ahead == 1 :
-        comp = random.randint(1,4)
-        print("What You Want to Choose ? :")
-
-        you = int(input("\n1 -> Rock\n2 -> Paper\n3 -> Scissor \nChoice : ")) 
-        print(f"Computer Have Choosen {options[comp]}")
-        print(f"You Have Choosen {options[you]}")
-
-    elif go_ahead == 0 :
+    print("Enter Your Choice : ")
+    go_ahead = int(input("\n1 -> Play \n0 -> Exit\nChoice : "))
+    
+    if go_ahead == 1:
+        
+        comp = random.randrange(1,4)
+        
+        print(f"Enter Your Choice : \n 1 = {options[1]}\n 2 = {options[2]}\n 3 = {options[3]}\n")
+        you = int(input("\nEnter Your Choice : "))
+        
+        print(f"Computer Choice = {options[comp]}\nYour Choice = {options[you]}")
+        
+        print(winner(comp,you))
+        print("-" * 40)
+        
+    elif go_ahead == 0:
         exit()
-
+        
     else :
         print("Enter Valid Choice...\n")
