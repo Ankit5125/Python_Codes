7342060c0ca45f0b34c1bac20e670f36796aa752
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-26 13:36:50 +0000

    created a lot of things

---
+++ File IO/p1.py
@@ -0,0 +1,7 @@
+# Read mode cant edit file....
+
+file = open("F1.txt")
+
+print(file.read())
+
+file.close()
---
+++ File IO/p2.py
@@ -0,0 +1,7 @@
+# Writing mode cant read the file....
+
+file = open("F2.txt","w")
+
+print(file.write("This is the Example of Printing In File..."))
+
+file.close()
