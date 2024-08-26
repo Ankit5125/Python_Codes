64fdfb742b98b226eb8adc0fddd68c7751f0a7d4
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-26 18:12:28 +0000

    done

--- My_Codes/Greatest Common Devisior.py
+++ My_Codes/Greatest Common Devisior.py
@@ -5,7 +5,7 @@ num2 = int(input("Enter Number 2 : "))
 max = 1 
 
 for i in range(1,min(num1,num2)+1):
-    if (num1 / i == 0) and (num2 / i == 0):
+    if (num1 % i == 0) and (num2 % i == 0):
         max = i
 
 print(max)
