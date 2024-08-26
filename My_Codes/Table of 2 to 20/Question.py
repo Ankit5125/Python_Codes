b88737c05bfb2d3858b23b1125706bc6a47fe6d0
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-26 16:08:39 +0000

    created tables folder

---
+++ My_Codes/Table of 2 to 20/Question.py
@@ -0,0 +1,12 @@
+'''
+you have to print table of 2 to 20
+each table must be in unique file
+'''
+
+for i in range(1,21):
+    f = open(f"{i}.txt","w")
+
+    for j in range(1,11):
+        f.write(f"{i} * {j} = {i * j}\n")
+
+    print(f"Table of {i} Created Successfully...")
