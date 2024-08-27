6f3e595d03167164bfecc0f9ef515d4978f23eda
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-27 12:47:03 +0000

    udiddj

--- Projects/Guess The Number Game.py
+++ Projects/Guess The Number Game.py
@@ -1,19 +1,36 @@
 import random
 
-guess = random.randint(1,100)
-count = 0 
-
-while True : 
-    prompt = int(input("Enter Your Guesed Number : "))
-
-    if prompt == guess :
-        print("Congratulations !!\n You Found Answer in : ", count, "Attempts")
-        exit()
+def play():
+    guess = random.randint(1,100)
+    count = 0 
     
-    else :
-        print("Wrong Answer...")
-        if prompt > guess :
-            print("Hint : Guess Small Number...")
+    while True : 
+        prompt = int(input("\nEnter Your Guesed Number : "))
+        
+        if prompt == guess :
+            print("\nCongratulations !!\n You Found Answer in : ", count, "Attempts\n")
+            break
+            
         else :
-            print("Hint : Guess Bigger Number...")
-        count+=1
+            print("Wrong Answer...")
+            
+            if prompt > guess :
+                print("Hint : Guess Small Number...\n")
+            
+            else :
+                print("Hint : Guess Bigger Number...\n")
+            
+            count+=1
+
+while True :
+    go_ahead = int(input("\n\nEnter Choice : \n 1 - > Play Ahead\n 0 -> Exit \nChoice : "))
+    
+    if go_ahead == 1 :
+        play()
+        
+    elif go_ahead == 0 :
+        print("\nThank You...\n")
+        exit()
+        
+    else :
+        print("\nPlease Enter Valid Choice...\n")
