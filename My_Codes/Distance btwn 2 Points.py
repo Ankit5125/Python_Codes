fd49e669de5089fecc5755b62e9ea0f1dd57a11e
Ankit Savaliya <167634896+Ankit5125@users.noreply.github.com>
2024-08-27 05:00:43 +0000

    fsaffsaf

---
+++ My_Codes/Distance btwn 2 Points.py
@@ -0,0 +1,12 @@
+'''
+you have two points 
+(x1,y1) and (x2,y2)
+find diistance between them
+'''
+
+p1 = (10, 30)
+p2 = (50, 90)
+
+distance = ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 )**0.5
+
+print(distance)
